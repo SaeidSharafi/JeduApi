6dcab731b142e98ab8ad5f6dc31b5227994c4e3c
saeed <saeed_sharafi12@yahoo.com>
2025-05-17 13:55:34 +0330

    feat: add AdvanceEnum trait for enhanced enum functionality

---
+++ app/Traits/AdvanceEnum.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Traits;
+
+trait AdvanceEnum
+{
+    public function translate(): string
+    {
+        $key =  class_basename($this);
+        return __("enums.{$key}.{$this->value}");
+    }
+
+    /**
+     * Get all status values as an array.
+     *
+     * @return array<string>
+     */
+    public static function getAllValues(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    /**
+     * Get all status names (enum case names) as an array.
+     *
+     * @return array<string>
+     */
+    public static function getAllNames(): array
+    {
+        return array_column(self::cases(), 'name');
+    }
+
+    /**
+     * Get Key-Value Pairs for Enum
+     *
+     * @return array<string>
+     */
+    public function getKeyValuePairs(): array
+    {
+        $keyValuePairs = [];
+        foreach (self::cases() as $key => $value) {
+            $keyValuePairs[$value->value] = $value->traslate();
+        }
+        return $keyValuePairs;
+    }
+
+    /**
+     * get Value Label array
+     *
+     * @return array<string>
+     */
+    public function getValueLabel(): array
+    {
+        return array_map(
+            fn($case) => [
+                'value' => $case->value,
+                'label' => $case->traslate(),
+            ],
+            self::cases()
+        );
+    }
+}
