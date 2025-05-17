e2958429f1787eb8ef1987dc73687a99701cc6d7
saeed <saeed_sharafi12@yahoo.com>
2025-05-17 21:02:07 +0330

    feat: update courses table migration to use CourseStatusEnum for status field and set default to PUBLISHED

--- database/migrations/2025_05_17_091826_create_courses_table.php
+++ database/migrations/2025_05_17_091826_create_courses_table.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Enums\CourseStatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,7 +21,7 @@ return new class extends Migration
             $table->string('meta_title')->nullable();
             $table->string('meta_description')->nullable();
             $table->string('meta_keywords')->nullable();
-            $table->enum('status', App\Enums\CourseStatusEnum::getAllValues())->default('active');
+            $table->enum('status', CourseStatusEnum::getAllValues())->default(CourseStatusEnum::PUBLISHED->value);
             $table->foreignId('created_by')->nullable()->constrained('admins', 'id')->nullOnDelete();
             $table->timestamps();
         });
