0cd10759511b2e1bbdc04ef3ef24ab08e1e58f71
saeed <saeed_sharafi12@yahoo.com>
2025-05-20 15:44:12 +0330

    fix: update namespace for GitHub deployer webhook route

--- routes/Api/V1/api.php
+++ routes/Api/V1/api.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-Route::webhooks('webhooks/github-deployer', 'github-deployer');
+\Illuminate\Support\Facades\Route::webhooks('webhooks/github-deployer', 'github-deployer');
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 require __DIR__.'/course.php';
