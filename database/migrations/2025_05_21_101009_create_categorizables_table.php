933df1b9f7ef2f666523c0e6bfe45c7640db8fff
saeed <saeed_sharafi12@yahoo.com>
2025-05-29 10:07:17 +0330

    rename categorizables migration file

--- database/migrations/2025_05_21_101009_create_categorizables_table.php
+++ database/migrations/2025_05_21_101009_create_categorizables_table.php
@@ -10,7 +10,6 @@ return new class extends Migration
 {
     public function up(): void
     {
-
         Schema::create('categorizables', function (Blueprint $table) {
             $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
             $table->unsignedBigInteger('categorizable_id');
