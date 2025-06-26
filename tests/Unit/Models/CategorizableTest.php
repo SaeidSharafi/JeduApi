626af5e94b788c2c0ea08a80c67dbf1b5b68aa25
saeed <saeed_sharafi12@yahoo.com>
2025-06-26 13:58:04 +0330

    feat: add tests for categorizable relationships in Category and new Categorizable model

---
+++ tests/Unit/Models/CategorizableTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Category;
+use App\Models\Course;
+use App\Models\DigitalAsset;
+use App\Models\Product;
+
+test('to Array', function (): void {
+    $category = Category::factory()->create();
+    $product = Product::factory()->create();
+    $product->categories()->attach($category->id);
+    $categorizbale = \App\Models\Categorizable::first();
+    expect($categorizbale)->toBeInstanceOf(\App\Models\Categorizable::class)
+        ->and($categorizbale->toArray())
+        ->toEqual([
+            'category_id'        => $category->id,
+            'categorizable_id'   => $product->id,
+            'categorizable_type' => \App\Enums\MorphTypeEnum::PRODUCT->value,
+        ]);
+});
+test('categorizable realtionship', function () {
+    $category = Category::factory()->create();
+    $course = Course::factory()->create();
+    $course->categories()->attach($category->id);
+    $categorizable = \App\Models\Categorizable::first();
+    expect($categorizable)->toBeInstanceOf(\App\Models\Categorizable::class)
+        ->and($categorizable->categorizable)
+        ->toBeInstanceOf(Course::class)
+        ->and($categorizable->categorizable->id)
+        ->toEqual($course->id);
+});
--- tests/Unit/Models/CategoryTest.php
+++ tests/Unit/Models/CategoryTest.php
@@ -56,3 +56,36 @@ test('relation categorizable for Digital Assets', function (): void {
         ->and($category->digitalAssets->first()->id)
         ->toEqual($asset->id);
 });
+test('relation categorizable for seminars', function (): void {
+    $category = Category::factory()->create();
+    $seminar    = \App\Models\Seminar::factory()->create();
+    $category->seminars()->attach($seminar);
+
+    expect($category->seminars)
+        ->toHaveCount(1)
+        ->and($category->seminars->first())
+        ->toBeInstanceOf(\App\Models\Seminar::class)
+        ->and($category->seminars->first()->id)
+        ->toEqual($seminar->id);
+});
+test('relation categorizable', function (): void {
+    $category = Category::factory()->create();
+    $seminar    = \App\Models\Seminar::factory()->create();
+    $course    = \App\Models\Course::factory()->create();
+    $category->seminars()->attach($seminar);
+    $category->courses()->attach($course);
+
+    expect($category->categorizable)
+        ->toHaveCount(2);
+});
+test('relation product', function (): void {
+    $category = Category::factory()->create();
+    $product  = \App\Models\Product::factory()->create()->fresh();
+    $category->products()->attach($product);
+    expect($category->products)
+        ->toHaveCount(1)
+        ->and($category->products->first())
+        ->toBeInstanceOf(\App\Models\Product::class)
+        ->and($category->products->first()->id)
+        ->toEqual($product->id);
+});
