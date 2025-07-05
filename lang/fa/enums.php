eb2fe442befc4d4bb0d42866011d362a13f2fca9
saeed <saeed_sharafi12@yahoo.com>
2025-07-05 15:13:49 +0330

    feat: translations for enums

--- lang/fa/enums.php
+++ lang/fa/enums.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'PublicationStatusEnum' => [
+    'PublicationStatusEnum'     => [
         'draft'     => 'پیش‌نویس',
         'published' => 'منتشر شده',
         'archived'  => 'آرشیو',
@@ -14,16 +14,16 @@ return [
         'advanced'     => 'پیشرفته',
         'expert'       => 'کارشناس',
     ],
-    'GenderEnum' => [
+    'GenderEnum'                => [
         'male'   => 'مرد',
         'female' => 'زن',
     ],
-    'TermStatusEnum' => [
+    'TermStatusEnum'            => [
         'active'   => 'فعال',
         'inactive' => 'غیرفعال',
         'planning' => 'در حال برنامه‌ریزی',
     ],
-    'MorphTypeEnum' => [
+    'MorphTypeEnum'             => [
         'course'        => 'دوره',
         'seminar'       => 'سمینار',
         'digital_asset' => 'فایل',
@@ -33,19 +33,19 @@ return [
         'teacher'       => 'مدرس',
         'vendor'        => 'فروشنده',
     ],
-    'ProductableEnum' => [
+    'ProductableEnum'           => [
         'course'        => 'دوره',
         'seminar'       => 'سمینار',
         'digital_asset' => 'فایل',
     ],
-    'FulfillmentTypeEnum' => [
+    'FulfillmentTypeEnum'       => [
         'digital'           => 'دیجیتال',
         'physical'          => 'فیزیکی',
         'online_service'    => 'سرویس آنلاین',
         'offline_service'   => 'سرویس آفلاین',
         'in_person_service' => 'سرویس حضوری',
     ],
-    'DeliveryMethodEnum' => [
+    'DeliveryMethodEnum'        => [
         'direct_download'           => 'دانلود مستقیم',
         'live_session_bbb'          => 'جلسه زنده با BBB',
         'live_session_skyroom'      => 'جلسه زنده با Skyroom',
@@ -53,10 +53,58 @@ return [
         'video_platform_spotplayer' => 'پلتفرم ویدیویی SpotPlayer',
         'in_person'                 => 'حضوری',
     ],
-    'CivilIdTypeEnum' => [
-        'national_code' => 'کد ملی',
-        'passport'      => 'پاسپورت',
-        'immigrant_code'         => 'کد اتباع',
+    'CivilIdTypeEnum'           => [
+        'national_code'  => 'کد ملی',
+        'passport'       => 'پاسپورت',
+        'immigrant_code' => 'کد اتباع',
     ],
-
+    'EducationLevelEnum'        => [
+        'student'       => 'دانش‌آموز',
+        'under_diploma' => 'زیردیپلم',
+        'diploma'       => 'دیپلم',
+        'associate'     => 'کاردانی',
+        'bachelor'      => 'کارشناسی',
+        'master'        => 'کارشناسی ارشد',
+        'doctorate'     => 'دکتری',
+    ],
+    'EducationStatusEnum'       => [
+        'student'   => 'دانشجو',
+        'college'   => 'دانشگاه',
+        'graduated' => 'فارغ‌التحصیل',
+        'employed'  => 'شاغل',
+    ],
+    'OrderItemStatusEnum'       => [
+        'active'    => 'فعال',
+        'cancelled' => 'لغو شده',
+        'refunded'  => 'بازپرداخت شده',
+    ],
+    'OrderItemPaymentTypeEnum'  => [
+        'pre_payment'  => 'پیش‌پرداخت',
+        'full_payment' => 'پرداخت کامل',
+        'invoice'      => 'صورتحساب',
+    ],
+    'OrderPaymentStatusEnum'    => [
+        'pending'            => 'در انتظار',
+        'partially_paid'     => 'پرداخت جزئی',
+        'paid'               => 'پرداخت شده',
+        'refunded'           => 'بازپرداخت شده',
+        'partially_refunded' => 'بازپرداخت جزئی',
+    ],
+    'OrderStatusEnum'           => [
+        'pending'    => 'در انتظار',
+        'processing' => 'در حال پردازش',
+        'completed'  => 'تکمیل شده',
+        'cancelled'  => 'لغو شده',
+        'failed'     => 'ناموفق',
+        'refunded'   => 'بازپرداخت شده',
+    ],
+    'PaymentMethodEnum'         => [
+        'bank_transfer'  => 'انتقال بانکی',
+        'online_gateway' => 'درگاه آنلاین',
+    ],
+    'PaymentStatusEnum'         => [
+        'pending'   => 'در انتظار',
+        'completed' => 'تکمیل شده',
+        'failed'    => 'ناموفق',
+    ],
 ];
