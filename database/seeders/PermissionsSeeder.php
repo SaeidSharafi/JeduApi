5581e53b40c93070d80af39106eb3904b403a039
saeed <saeed_sharafi12@yahoo.com>
2025-09-11 15:00:04 +0330

    feat: simplify PermissionsSeeder by removing logging and directly syncing permissions

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -4,21 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Log;
-use Spatie\Permission\PermissionRegistrar;
 
 class PermissionsSeeder extends Seeder
 {
     public function run(): void
     {
-        $pid = getmypid();
-        $processLogger = Log::build([
-            'driver' => 'single',
-            'path' => storage_path("logs/test-debug-{$pid}.log"),
-        ]);
-
-        $processLogger->info("Seeder is running!");
-        // Sync the permissions
         Artisan::call('permissions:sync --guard=staff');
     }
 }
