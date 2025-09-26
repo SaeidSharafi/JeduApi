72f52299f13c2b328c10097a6b6775a17a1cf35e
saeed <saeed_sharafi12@yahoo.com>
2025-09-26 15:55:57 +0330

    feat: make SKU optional for ProductDeliveryOption and auto-generate if not provided

--- app/Actions/Admin/ProductDeliveryOption/CreateProductDeliveryOptionAction.php
+++ app/Actions/Admin/ProductDeliveryOption/CreateProductDeliveryOptionAction.php
@@ -8,17 +8,21 @@ use App\Data\Admin\ProductDeliveryOption\ProductDeliveryOptionCreateData;
 use App\Events\ProductCacheInvalidated;
 use App\Models\Product;
 use App\Models\ProductDeliveryOption;
+use App\Services\SkuGeneratorService;
 use Illuminate\Support\Facades\DB;
 
 final readonly class CreateProductDeliveryOptionAction
 {
+    public function __construct(private SkuGeneratorService $skuGenerator) {}
     /**
      * Execute the action.
      */
     public function handle(ProductDeliveryOptionCreateData $data, Product $product): ProductDeliveryOption
     {
         $pdo = DB::transaction(function () use ($data, $product): ProductDeliveryOption {
+            $baseSku = $this->skuGenerator->generateBaseSku($data, $product);
             $pdoData = $data->except('teachers')->toArray();
+            $pdoData['sku'] = data_get($pdoData,'sku') ?? $baseSku;
             $pdo     = $product->productDeliveryOptions()->create($pdoData)->fresh();
             $pdo->teachers()->attach($data->teachers);
 
